d2caac97b90fc7d5676ac08d77eb7741015673ac
YesiO2024 <yesicadanielaochoa@gmail.com>
2024-06-29 02:01:57 -0300

    agregué el alerta en totoc y modifiqué el inicio

--- js/function.js
+++ js/function.js
@@ -1,1 +1,13 @@
-alert(`Te estas redireccionando a otra pagina web`)
+document.addEventListener('DOMContentLoaded', function() {
+    var enlaces = document.querySelectorAll('.enlace-alerta');
+
+    enlaces.forEach(function(enlace) {
+        enlace.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevenir la acción por defecto del enlace
+            alert('Estás a punto de dirigirte a otro sitio web.');
+            window.open(this.href, '_blank'); // Abrir el enlace en una nueva pestaña
+        });
+    });
+});
+
+//alert(`Te estas redireccionando a otra pagina web`)
