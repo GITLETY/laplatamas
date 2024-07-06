4f2596ba0d67d392affc2faae1ed3c8d8e6d4498
YesiO2024 <yesicadanielaochoa@gmail.com>
2024-07-06 01:16:34 -0300

    FUNCION MEJORADA DE SUSCRIBIR EN INICIO

--- js/index.js
+++ js/index.js
@@ -1,17 +1,52 @@
 
-
 function newsletter(){
+     if (confirm('Podes recibir contenido preferencial ¿Quieres suscribirte a nuestro Newsletter?')){
+       
+        let nombre
+        let apellido
+        let email
 
-    if (confirm('Podes recibir contenido preferencial ¿Quieres suscribirte a nuestro Newsletter?')){
-        let nombre = prompt('Por favor, ingresa tu nombre:');
-        if (nombre === null) return;
+        do { 
+            nombre = prompt('Por favor, ingresa tu nombre:');
+            console.log(nombre);
+            if (nombre === ''){
+            alert("Debes ingresar tu nombre para continuar");
+            }
+        }while(nombre === '')
 
-        let apellido = prompt('Por favor, ingresa tu apellido:');
-        if (apellido === null) return;
+     if(nombre !== null){
+        
+        do { 
+            apellido = prompt('Por favor, ingresa tu apellido:');
+            if (apellido === ''){
+            alert("Debes ingresar tu apellido para continuar.");
+            }
+        }while(apellido === '')
     
-        let gmail = prompt('Por favor, ingresa tu Gmail:');
-        if (gmail === null) return;
 
+      if(apellido !== null){ 
+    
+        do { email = prompt('Por favor, ingresa tu mail:');
+        if (email === '') {
+            alert("Debes ingresar tu email para continuar");
+            }
+        }while(email === '')
 
-    }
+      if(email !== null){
+
+        console.log("Nombre:", nombre);
+        console.log("Apellido:", apellido);
+        console.log("Email:", email);
+
+        let saludo = `Bienvenido ${nombre} ${apellido}a nuestra comunidad. Pronto recibirás un mail con contenido exclusivo.`;
+        alert(saludo)
+
+        console.log("Saludo mostrado:", saludo);
+
+       } } }
+   if(nombre === null || apellido === null || email === null){ alert( "No se ha podido suscribir"); }
+ } else {
+        console.log("Usuario canceló la suscripción al newsletter."); 
+      }
 }
+
